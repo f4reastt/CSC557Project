b252d057132443868ec9bb748b5c03ba56110ef5
Wan Alif <alifhidhir@gmail.com>
2022-07-22 04:57:54 +0800

    Update Registered_CourseActivity.java

--- app/src/main/java/com/main/application/Registered_CourseActivity.java
+++ app/src/main/java/com/main/application/Registered_CourseActivity.java
@@ -10,5 +10,8 @@ public class Registered_CourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered_course);
+
+
+
     }
 }
